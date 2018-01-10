909b335bf9d914b25a8c59e158fac4b5d8b43286
stephen macleod <steve.macleod@clockwork.ca>
2018-01-09 23:05:14 -0700

    fix evil relations pointer to slice, adjust err declaration controller CRUD methods to accomodate optional generation of controller extension points.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 
-	projectPath := flag.String("p", "/exp/usrgen7", "project path starting in &GOPATH/src")
+	projectPath := flag.String("p", "/exp/usrgen8", "project path starting in &GOPATH/src")
 	modelFile := flag.String("m", "", "model file relative to application base directory")
 	modelDirectory := flag.String("md", "", "process all model files in the specified directory")
 
