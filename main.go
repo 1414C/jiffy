a1a55fdfb52c7403dcdabfef4a59c7791aa7cd09
stephen macleod <steve.macleod@clockwork.ca>
2018-01-17 20:57:14 -0700

    refactor hasOne to new standard

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 
-	projectPath := flag.String("p", "/exp/usrgen96", "project path starting in &GOPATH/src")
+	projectPath := flag.String("p", "/exp/usrgen97", "project path starting in &GOPATH/src")
 	modelFile := flag.String("m", "", "model file relative to application base directory")
 	modelDirectory := flag.String("md", "", "process all model files in the specified directory")
 
